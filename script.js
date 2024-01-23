6bce93523b4018019113977c80d632e67a77db41
denisesarona <135094209+denisesarona@users.noreply.github.com>
2024-01-23 19:57:47 +0800

    d

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
+// Popover Toggle
+document.addEventListener('DOMContentLoaded', function () {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [];
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-        const popoverList = [];
-
-        popoverTriggerList.forEach(function (popoverTriggerEl) {
-            const popover = new bootstrap.Popover(popoverTriggerEl, {
-                trigger: 'manual',
-                html: true,
-                content: function () {
-                    return $('#popover-content').html();
-                }
-            });
-
-            popoverTriggerEl.addEventListener('click', function () {
-                if (popover._popper) {
-                    popover.dispose(); // Close the popover
-                } else {
-                    popover.show(); // Show the popover
-                }
-            });
-
-            popoverList.push(popover);
+    popoverTriggerList.forEach(function (popoverTriggerEl) {
+        const popover = new bootstrap.Popover(popoverTriggerEl, {
+            trigger: 'manual',
+            html: true,
+            content: function () {
+                return $('#popover-content').html();
+            }
         });
+
+        popoverTriggerEl.addEventListener('click', function () {
+            if (popover._popper) {
+                popover.dispose(); // Close the popover
+            } else {
+                popover.show(); // Show the popover
+            }
+        });
+
+        popoverList.push(popover);
     });
+});
 
